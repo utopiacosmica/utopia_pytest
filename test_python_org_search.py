7238b088f3236eb0932ff09ccf59afb822ec4781
Roma <utopiacosmica@icloud.com>
2017-05-31 13:46:22 -0700

    4

---
+++ test_python_org_search.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Chrome('driver_mac/chromedriver')
+driver.maximize_window()
+
+def test_python_org_search():
+        driver.get("http://www.python.org")
+        elem = driver.find_element_by_name("q")
+        elem.send_keys("pycon")
+        elem.send_keys(Keys.RETURN)
+        assert "No results found." not in driver.page_source
+        driver.close()
